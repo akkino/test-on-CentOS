16799d2f26c87021a68eaa7a606421d6d731ef84
akkino <git@github.com>
2019-02-07 01:13:31 +0900

    リスト2-15,16：リクエストとレスポンス

--- mylesson/app/Http/Controllers/HelloController.php
+++ mylesson/app/Http/Controllers/HelloController.php
@@ -3,32 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
-global $head, $style, $body, $end;
-$head = '<html><head>';
-$style = <<<EOF
-<style>
-body {font-size:16px; color:#999; }
-h1 { font-size:100px; text-align:right; color:#eee;
-  margin:-40px; 0px -50px 0px; }
-</style>
-EOF;
-$body = '</head><body>';
-$end = '</body></head>';
-
-function tag($tag,$txt) {
-    return "<{$tag}>" . $txt . "</{$tag}>";
-}
+use Illuminate\Http\Response;
 
 class HelloController extends Controller
 {
 
-    public function __invoke() {
+    public function index(Request $request, Response $response) {
 
-      return <<<EOF
+$html =  <<<EOF
 <html>
 <head>
-<title>Hello</title>
+<title>Hello/Index</title>
 <style>
 body {font-size:16px; color:#999; }
 h1 { font-size:100px; text-align:right; color:#eee;
@@ -36,10 +21,15 @@ h1 { font-size:100px; text-align:right; color:#eee;
 </style>
 </head>
 <body>
-  <h1>Single</h1>
-  <p>これは、シングルアクションコントローラのアクションです。</p>
+  <h1>Hello</h1>
+  <h3>Request</h3>
+  <pre>{$request}</pre>
+  <h3>Response</h3>
+  <pre>{$response}</pre>
 </body>
 </html>
 EOF;
+        $response->setContent($html);
+        return $response;
     }
 }
